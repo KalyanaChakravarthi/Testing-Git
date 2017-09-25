64f5b18f46e1c90f6a12ed2d1b0022e90b450267
KalyanaChakravarthi <31840402+KalyanaChakravarthi@users.noreply.github.com>
2017-09-25 11:56:46 +0530

    Adding new file
    Adding new File

---
+++ Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class A {
+protected:
+	int x;
+public:
+	A() {
+		x = 0;
+	}
+	A(int i) {
+		x = i;
+	}
+};
+
+class B : public A {
+public:
+	B(int i) : A(i) {}
+	void print() {
+		cout << "x:" << x << endl;
+	}
+};
+
+class main {
+public:
+	main() {
+		cout << "ctor" << endl;
+	}
+	~main() {
+		cout << "dctor" << endl;
+	}
+}m;
+
+int main() {
+	cout << "in main" << endl;
+	B b(10);
+	b.print();
+	system("pause");
+	return 1;
+}
